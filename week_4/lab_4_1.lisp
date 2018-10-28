73b779d63b6745427f3e77b9bbbe3ac8fb26f8d5
ilia <iluya.kalinichev@gmail.com>
2018-10-28 22:14:55 +0300

    [WEEK-4] recursively summ odd numbers in list (#1)
    * [WEEK-4] recursively summ odd numbers in list

---
+++ week_4/lab_4_1.lisp
@@ -0,0 +1,46 @@
+(defun count-of-odds (lst)
+    "Checking that all elemets in list is odd."
+         (cond
+             ( ;IF null return 0 and quit
+                 (null lst) 0
+             )
+             ( ;IF number
+                 (numberp lst)
+                     (cond
+                         ( ;AND even, return 0
+                             (evenp lst) 0
+                         )
+                         ( ;AND odd, return 1
+                             (oddp lst) 1
+                         )
+                     )
+             )
+             ( ;ELSE recursively call and split for first element
+               ;and tail
+                 (listp lst) (+ (count-of-odds (car lst)) (count-of-odds (cdr lst)))
+             )
+         )
+)
+
+(defun sum-odds (lst)
+    "Sum numbers if list contains only odd elemets."
+    (defvar result-list nil)
+    (defvar index 1)
+    (dolist (i lst)
+        (cond
+            ( ;IF list than recursively call
+                (listp i) (sum-odds i)
+            )
+            ( ;IF number is last and list of odd elements
+                (and (= index (list-length lst)) (= (count-of-odds lst) (list-length lst)))
+                    (print i) ; не заходит :(
+                    (setf (nth (- index 1) lst) (apply '+ lst))
+            )
+            (t ;ELSE increment index
+                (push i result-list)
+            )
+        )
+        (incf index)
+    )
+    (values result-list)
+)
