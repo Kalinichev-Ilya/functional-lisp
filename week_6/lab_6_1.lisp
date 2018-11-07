59523aa2e5ccf2aacba3a9b356ed5c082c0335ef
ikzekly <iluya.kalinichev@gmail.com>
2018-11-08 02:22:13 +0300

    [WEEK-6] found count of matching entries

--- week_6/lab_6_1.lisp
+++ week_6/lab_6_1.lisp
@@ -1,15 +1,49 @@
+; функции обработки Hash start----
+(defun set% (h key val &rest other-pairs)
+  (let ((pairs (append (list key val) other-pairs)) k v)
+      (hash-table-add-list h pairs)))
+
+; считает кол-во одинаковых элементов в hash
+(defun hash++ (h key &optional (n 1))
+    (let ((val (gethash key h)))
+        (cond
+            ((or (null val) (not (numberp val)))
+                (set% h key n))
+            (t (set% h key (+ val n))))
+        (gethash key h)))
+
+; добавляет в hash лист
+(defun hash-table-add-list (h lst &aux k v)
+    (loop
+        (if (null lst) (return))
+        (setf k (pop lst))
+        (if (null lst) (return))
+        (setf v (pop lst))
+        (setf (gethash k h) v))
+    h)
+
+; формирует hash
+(defun hash-table-key-value-pairs (h)
+    (let (pairs)
+        (maphash #'(lambda (k v)
+            (push (list k v) pairs)) h)
+        (nreverse pairs)))
+; ----end
+
 ;;;; Кто_звонил;Кому_звонили;Длительность остальные игнорировать
 ;;;; Номер из 4х символов;
 ;;;; Кому начинается на +49 и +39
 
-(defparameter *vrps* '(
-    "1101;+79119989911;122"
-    "+49231114563;1102;347"
-    "1101;+420023334521;134"
-    "1102;+49023334521;811"
-    "ERR01:1234;;;0;0"
-    "1101;+390145211212;93"
-    "1101;+49023334521;756"
+(defparameter *vrps*
+    '(
+        "1101;+79119989911;122"
+        "+49231114563;1102;347"
+        "1101;+420023334521;134"
+        "1102;+49023334521;811"
+        "1102;+49023334521;81"
+        "ERR01:1234;;;0;0"
+        "1101;+390145211212;93"
+        "1101;+49023334521;756"
     )
 )
 
@@ -21,33 +55,38 @@
 ;;;; - reduce
 ;;;; вывести внустренний номер сотрудник который звонил больше всего
 
-; проверка на содержание цифры
-(defun is-liquid-number (c)
-    (and (find c "01"
-        :test #'string-equal) t))
-
-; проверка на номер оператора
+; маска на номер оператора
 (defun is-operator-phone (v)
-    (print v)
     (and (>= (length v) 4)
         (char= #\; (elt v 4))
-        (is-liquid-number (elt v 0))
-        (is-liquid-number (elt v 1))
-        (is-liquid-number (elt v 2))
-        (is-liquid-number (elt v 3))))
+        (digit-char-p (elt v 0))
+        (digit-char-p (elt v 1))
+        (digit-char-p (elt v 2))
+        (digit-char-p (elt v 3))))
 
-; проверка на нужный номер
-(defun is-liquid-recipient (number prefix)
-        (equal (subseq number 0 3) prefix))
+; отдает Т для номеров начинающихся с +49 и +39
+(defun is-liquid-recipient (v)
+    (or
+        (equal (subseq (recipient-number v) 0 3) "+49")
+        (equal (subseq (recipient-number v) 0 3) "+39")))
 
-# делит строку по ;
-(defun number-before-semicolon (v)
+; достает номер оператора
+(defun operator-number (v)
+    (subseq v 0 (position #\; v)))
+
+; достает номер абонента
+(defun recipient-number (v)
     (subseq v (1+ (position #\; v))))
 
-(defun get-operators-list (lst)
-    (mapcar
-        #'number-before-semicolon
+; очищенный список
+; TODO вырезать номер абонента
+(defun filtered-calls-list (lst)
     (remove-if
         (complement #'is-operator-phone)
-        lst)))
+            (remove-if
+                (complement #'is-liquid-recipient) lst)))
 
+; TODO сделать hash (номер оператора . кол-во минут)
+; TODO сложить кол-во минут по номеру оператора
+; TODO отсортировать по убыванию
+; TODO вернуть номер оператора из нулевой позиции
